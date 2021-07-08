c88ca22dc07e0f51f9e6a02dbad12665e618c258
Cam <kienhoanh.le@gmail.com>
2021-07-08 16:06:10 +0700

    add Update function to App

--- src/index.js
+++ src/index.js
@@ -35,7 +35,16 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  updateApp = require('update-electron-app');
+
+  updateApp({
+    updateInterval: "1 hour",
+    notifyUser: true
+  });
+
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
