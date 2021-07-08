182e14ca3b3495785b1b7e92b608874191d04680
Cam <kienhoanh.le@gmail.com>
2021-07-08 16:51:33 +0700

    remove updater

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const {
 const path = require('path');
 const { electron } = require('process');
 
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
+
 
 const createWindow = () => {
   // Create the browser window.
